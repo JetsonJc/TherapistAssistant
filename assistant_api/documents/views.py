5230c412a5acf6487631b13e4ed0cc174bf4a8ca
Jessica Lechuga <JetsonJc>
2021-05-08 00:59:09 -0500

    Feat(documentos)

--- assistant_api/documents/views.py
+++ assistant_api/documents/views.py
@@ -7,12 +7,13 @@ from utility.storage import post_document
 
 class DocumentDetail(APIView):
     @swagger_auto_schema(
-        request_body='completar',
+        #request_body='completar',
         responses={
             status.HTTP_200_OK: "response"
         }
     )
     def post(self, request, format=None):
         file_obj = request.FILES['file']
-        post_document("documents/1/archivo1.docx", file_obj)
+        name = "1/video"
+        post_document(name, file_obj)
         return Response(data={"detail":"ok"}, status=status.HTTP_200_OK)
